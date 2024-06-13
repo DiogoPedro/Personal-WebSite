069dfb85dff22e8b016ac10597e8516ac14bf3c8
Diogo França <diogo.franca@vtb.solutions>
2024-06-12 21:17:13 -0300

    Correção de background, criação de Publication.tsx

--- src/components/HomePage/Publication.tsx
+++ src/components/HomePage/Publication.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import UfpeImg from "../../assets/ufpe.png";
+import { Image } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
 const Publication: React.FC = () => {
   const spanRef = React.useRef<HTMLDivElement>(null);
@@ -22,34 +24,85 @@ const Publication: React.FC = () => {
   }, []);
 
   return (
-    <div className="bg-publication">
-      <div className="trapezoid-quaternary">
-        <h3 className="title-home text-center">
-          I am a web developer passionate about{" "}
-          <span className="text-color">
-            technolog
-            <span id="position-publication" ref={spanRef}>
-              y
-            </span>
-            .
-          </span>
-        </h3>
-        <div
-          className="hyphen"
-          style={{
-            position: "absolute",
-            top: `${spanPosition.top + 5}px`,
-            left: `${spanPosition.left}px`,
-          }}></div>
-        <div className="mt-5">
-          <p className="title-content">Systems integration using artificial intelligence.</p>
-          <p className="title-content">Degree in Computer Engineering from the Federal University of Pernambuco.</p>
-        </div>
-        <div className="d-flex flex-row-reverse">
-          <img src={UfpeImg} height="60px" alt="UFPE" className="ufpe-img" />
+    <>
+      <div className="bg-publication">
+        <div className="trapezoid-quaternary">
+          <h3 className="text-center text-light" style={{ fontSize: "4em", paddingTop: "1.3em" }}>
+            Publications created by me.
+          </h3>
+          <div>
+            <Card style={{ width: "18em", marginTop: "2em", marginLeft: "3em" }}>
+              <Card.Img
+                variant="bottom"
+                src={UfpeImg}
+                style={{
+                  width: "100%",
+                  height: "180px",
+                  objectFit: "contain",
+                  margin: "auto",
+                }}
+              />{" "}
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18em", marginTop: "5em", marginLeft: "23em" }}>
+              <Card.Img
+                variant="bottom"
+                src={UfpeImg}
+                style={{
+                  width: "100%",
+                  height: "180px",
+                  objectFit: "contain",
+                  margin: "auto",
+                }}
+              />{" "}
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18em", marginTop: "5em", marginLeft: "43em" }}>
+              <Card.Img
+                variant="bottom"
+                src={UfpeImg}
+                style={{
+                  width: "100%",
+                  height: "180px",
+                  objectFit: "contain",
+                  margin: "auto",
+                }}
+              />{" "}
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card style={{ width: "18em", marginTop: "5em", marginLeft: "53em" }}>
+              <Card.Img
+                variant="bottom"
+                src={UfpeImg}
+                style={{
+                  width: "100%",
+                  height: "180px",
+                  objectFit: "contain",
+                  margin: "auto",
+                }}
+              />{" "}
+              <Card.Body>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
