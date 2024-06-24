9457ca2bfe4571a865cf060887ac354696c690e6
Diogo França <diogo.franca@vtb.solutions>
2024-06-23 21:18:40 -0300

    Ajustes dos cards.

--- src/components/HomePage/Capabilities.tsx
+++ src/components/HomePage/Capabilities.tsx
@@ -1,60 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineWeb } from "react-icons/md";
 import { FaCog, FaServer } from "react-icons/fa";
 import { RiSignalTowerFill } from "react-icons/ri";
 
 const Capabilities: React.FC = () => {
+  const [iconColors, setIconColors] = useState({
+    web: "white",
+    cog: "white",
+    server: "white",
+    signal: "white",
+  });
+
+  const changeIconColor = (icon: string, color: string) => {
+    setIconColors((prev) => ({ ...prev, [icon]: color }));
+  };
+
   return (
-    <div className="bg-secondary mt-3 ml-3">
-      <div className="row row-cols-1 row-cols-md-4 g-3">
-        {/* Primeiro Card */}
-        <div className="col">
-          <div className="card-capabilities d-flex pt-3">
+    <div className="row">
+      {/* Primeiro Card */}
+      <div className="col-3">
+        <div className="d-flex justify-content-center">
+          <div
+            className="card-capabilities d-flex pt-3"
+            onMouseEnter={() => changeIconColor("web", "#9780e4")}
+            onMouseLeave={() => changeIconColor("web", "white")}>
             <div className="d-flex justify-content-center col-3">
-              <MdOutlineWeb className="w-100" size={30} color="white" />
+              <MdOutlineWeb size={30} color={iconColors.web} />
             </div>
             <div className="col-9">
-              <h4 className="text-light">Web Apps</h4>
-              <p className="text-light">Build full stack web apps with React, Blazor, Navilla.</p>
+              <h4>Web Apps</h4>
+              <p>Build web apps with React, AspNet MVC, Blazor and Navilla.</p>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* Segundo Card */}
-        <div className="col">
-          <div className="card-capabilities d-flex pt-3">
+      {/* Segundo Card */}
+      <div className="col-3">
+        <div className="d-flex justify-content-center">
+          <div
+            className="card-capabilities d-flex pt-3"
+            onMouseEnter={() => changeIconColor("cog", "#9780e4")}
+            onMouseLeave={() => changeIconColor("cog", "white")}>
             <div className="d-flex justify-content-center col-3">
-              <FaCog className="w-100" size={30} color="white" />
+              <FaCog size={30} color={iconColors.cog} />
             </div>
             <div className="col-9">
-              <h4 className="text-light">Automation</h4>
-              <p className="text-light">Automate processes using modern tools and technologies.</p>
+              <h4>APIs</h4>
+              <p>Develop APIs using Asp Net Core, Node and Express Node.</p>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* Terceiro Card */}
-        <div className="col">
-          <div className="card-capabilities d-flex pt-3">
+      {/* Terceiro Card */}
+      <div className="col-3">
+        <div className="d-flex justify-content-center">
+          <div
+            className="card-capabilities d-flex pt-3"
+            onMouseEnter={() => changeIconColor("signal", "#9780e4")}
+            onMouseLeave={() => changeIconColor("signal", "white")}>
             <div className="d-flex justify-content-center col-3">
-              <RiSignalTowerFill className="w-100" size={30} color="white" />
+              <RiSignalTowerFill size={30} color={iconColors.signal} />
             </div>
             <div className="col-9">
-              <h4 className="text-light">Networking</h4>
-              <p className="text-light">Design and implement robust networking solutions.</p>
+              <h4>Real Time</h4>
+              <p> Protocols / Bi-directional communication: gRPC, RabbitMQ, SignalR.</p>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* Quarto Card */}
-        <div className="col">
-          <div className="card-capabilities d-flex pt-3">
+      {/* Quarto Card */}
+      <div className="col-3">
+        <div className="d-flex justify-content-center">
+          <div
+            className="card-capabilities d-flex pt-3"
+            onMouseEnter={() => changeIconColor("server", "#9780e4")}
+            onMouseLeave={() => changeIconColor("server", "white")}>
             <div className="d-flex justify-content-center col-3">
-              <FaServer className="w-100" size={30} color="white" />
+              <FaServer size={30} color={iconColors.server} />
             </div>
             <div className="col-9">
-              <h4 className="text-light">Microservices</h4>
-              <p className="text-light">Architect scalable microservices-based applications.</p>
+              <h4>Microservices</h4>
+              <p>
+                Creation of microservices using Docker containers and Azure services, being Azure Kubernetes Service
+                etc.
+              </p>
             </div>
           </div>
         </div>
